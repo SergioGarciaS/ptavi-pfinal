9672cf7e34a63109fa7003f807a76a911792ef5e
Garcias <sergio.garcia.sti@gmail.com>
2017-12-29 16:59:32 +0100

    creación funcion log_maker

--- uaserver.py
+++ uaserver.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 """Clase (y programa principal) para un servidor de eco en UDP simple."""
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 import socketserver
 import sys
 import os
+import time
+
 
 class ConfigHandler(ContentHandler):
     """Funcion para leer el xml."""
@@ -12,6 +16,7 @@ class ConfigHandler(ContentHandler):
     def __init__(self):
         """Creación de lista de configuración."""
         self.config = []
+
     def startElement(self, name, attr):
         """Obtención de atributos del archivo de configuración."""
         if name == "account":
@@ -41,8 +46,8 @@ class ConfigHandler(ContentHandler):
             self.config.append(port_proxy)
 
        elif name == "log":
-            path = attr.get('path',"")
-            self.config.append(path)
+            log_path = attr.get('path',"")
+            self.config.append(log_path)
 
        elif name == "audio":
             Audio_path = attr.get('path',"")
@@ -51,6 +56,16 @@ class ConfigHandler(ContentHandler):
     def get_config(self):
         """ Devuelve la lista de configuración. """
         return self.config
+
+def log_maker(path, hora, Evento):
+    """ Función que escribe en el archivo log. """
+    Log_file = open(path, 'a')
+    hora = time.gmtime(float(hora))
+    Log_file.write(time.strftime('%Y%m%d%h%M%S',hora)
+    Evento = Evento.replace('\r\n', ' ') #Cambia los saltos por espacios.
+    fichero.write(Evento + '\r\n') #Añado el salto al final.
+    fichero.close()
+
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -72,7 +87,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
             print("El cliente nos manda " + line.decode('utf-8'))
 
-            if line.decode('utf-8').split(' ')[0] not in Methods:
+            if Method_Check not in Methods:
                 Answer = ('SIP/2.0 405 Method Not allowed' + '\r\n\r\n')
                 self.wfile.write(bytes(Answer, 'utf-8'))
 
@@ -81,10 +96,15 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 self.wfile.write(bytes(Answer, 'utf-8'))
 
             elif Method_Check == 'INVITE':
+                cuerpo = "Content-type: application/sdp\r\n"+
+                         "\r\nv=0\r\n" + "o=" + str(config[0]) +
+                         " " + str(config[2]) + "\r\ns=Pacticafinal\r\n" +
+                         "t=0\r\nm=audio " + str(config[4]) + " RTP\r\n\r\n"
+
                 Answer = ('SIP/2.0 100 Trying' + '\r\n\r\n' +
                           'SIP/2.0 180 Ringing' + '\r\n\r\n' +
                           'SIP/2.0 200 OK' + '\r\n\r\n' +
-                          'RTP: PUERTO al que recibir')
+                          cuerpo)
                 self.wfile.write(bytes(Answer, 'utf-8'))
 
             elif Method_Check == 'BYE':
@@ -92,7 +112,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 self.wfile.write(bytes(Answer, 'utf-8'))
 
             elif Method_Check == 'ACK':
-                toRun = ('mp32rtp -i ' + IP_Client + ' -p 23032 < ' + FILE)
+                toRun = ('mp32rtp -i ' + IP_Client + ' -p 23032 < ' + Audio_path)
                 print("Vamos a ejecutar", toRun)
                 os.system(toRun)
                 print(" *=====================*\n",
