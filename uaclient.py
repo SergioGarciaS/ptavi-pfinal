f8560b485b96db1ea5c1b4096ab1ae872ca14734
Garcias <sergio.garcia.sti@gmail.com>
2018-01-08 19:10:06 +0100

    pequeños cambios

--- uaclient.py
+++ uaclient.py
@@ -4,10 +4,12 @@
 
 import socket
 import sys
+import os
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from uaserver import ConfigHandler
 from uaserver import log_maker
+
 
 if len(sys.argv) == 4:
         CONFIG = sys.argv[1]
@@ -26,10 +28,15 @@ Methods = ['register','invite', 'bye']
 if Metodo not in Methods:
     sys.exit('Los metodos utilizados son: invite,register,bye')
 else:
+
+    Usuario = config[0]
+    PORT = config[6]
+    RTP_PORT = config[4]
+    IP_Client = config[2]
+    PORT_UA2 = config[3]   #REVISAR BIEN LO DE LOS PUERTOS
+    Audio_path = config[8]
+
     if Metodo == "register":
-        Usuario = config[0]
-        PORT = config[6]
-        PORT_UA2 = config[3]    #REVISAR BIEN LO DE LOS PUERTOS
         Expired = sys.argv[3]
         USER_M = Metodo.upper() + ' sip:' + Usuario + ':' + PORT_UA2
         Data = USER_M + ' ' + 'SIP/2.0\r\n'+ 'Expires: ' + Expired + '\r\n\r\n'
@@ -40,13 +47,13 @@ else:
         USER_M += 'Content-Type: application/sdp\r\n\r\n'
         Cuerpo = 'v=0\r\n' + 'o=' + config[0] + ' ' + config[2] + '\r\n'
         Cuerpo += 's=misesion\r\n' + 't=0\r\n'
-        Cuerpo += 'm=audio ' + config[3] + ' RTP\r\n\r\n'
+        Cuerpo += 'm=audio ' + config[4] + ' RTP\r\n\r\n'
         Data = USER_M + Cuerpo
 
     elif Metodo == 'bye':
         Destination = sys.argv[3]
         USER_M = Metodo.upper() + ' sip:' + Destination
-        Data = USER_M + ' SIP/2.0'
+        Data = USER_M + ' SIP/2.0\r\n\r\n'
 
     print(Data) #ATENCION TRAZA A QUITAR....
     IP_Proxy = config[5]
@@ -62,22 +69,37 @@ else:
             data = my_socket.recv(1024)
             print('Recibido -- ', data.decode('utf-8'))
             respuesta = data.decode('utf-8').split('\r\n\r\n')[0:3]
-
+            response = respuesta[0]
             if Metodo == 'invite' and respuesta == ['SIP/2.0 100 Trying',
-                                                    'SIP/2.0 180 Ringing',
-                                                    'SIP/2.0 200 OK']:
+                                                'SIP/2.0 180 Ringing',
+                                                'SIP/2.0 200 OK']:
+                Destination = sys.argv[3]
                 USER_M = 'ACK' + ' sip:' + Destination
                 Data = USER_M + ' ' + 'SIP/2.0\r\n\r\n'
                 print("Enviando:", USER_M)
                 my_socket.send(bytes(Data, 'utf-8'))
                 print("Socket terminado.")
 
-            elif Metodo == 'register':
-                if respuesta == 'SIP/2.0 401 Unauthorized':
-                    print(USER_M) # Aqui va la movida de la passwd.
-                else:
-                    Print("Registro correcto")
+                toRun = ('mp32rtp -i ' + IP_Client + ' -p ')
+                toRun += (RTP_PORT + ' < ' + Audio_path)
+                print("Vamos a ejecutar", toRun)
+                os.system(toRun)
+                print(" *=====================*\n",
+                      " |The file is send...|\n",
+                      "*=====================*\n")
 
+            elif Metodo == 'register':
+                response = response.split('\r\n')[0]
+                if response == 'SIP/2.0 401 Unauthorized':
+                    USER_M = Metodo.upper() + ' sip:'
+                    USER_M += Usuario + ':' + PORT_UA2
+                    Data = USER_M + ' ' + 'SIP/2.0\r\n'+ 'Expires: ' + Expired + '\r\n'
+                    Data += 'Authenticate:' + ' loquer\r\n\r\n'
+                    print("EnviandoR:", USER_M)
+                    my_socket.send(bytes(Data, 'utf-8'))
+                    print("Socket terminado.")
+                elif response == 'SIP/2.0 404 User Not Found':
+                    print("No es posible conectarse")
     except ConnectionRefusedError:
         print("Escribir en el log")
 
